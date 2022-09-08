f616dab6487c1e656eaf701e5302d1d6b9e56f8f
Laurel <lak3r@mtmail.mtsu.edu>
2022-09-08 18:48:57 +0000

    thinking about cluster things

---
+++ Codebase/single.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+singularity exec /nfshome/singularity-images/csci4850-2022-Fall.sif $1
+
+# $1 is the program
+# $2 is the corpus 
+# $3 is the training set
+# $4 is the testing set
--- Codebase/work_horse.sh
+++ Codebase/work_horse.sh
@@ -1,3 +1,11 @@
-# The connecty thing
+#!/bin/bash
+# $1 is the number of times to run each combo
 
-#
+for task in 'SG' 'SA' 'FC' 'NF'
+do
+	for ((x=0; x<$1; x++))
+	do
+		echo $task 
+		echo $x
+	done
+done
