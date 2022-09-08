eb5143c28e45a2771c32fab69702858acbc3d933
Laurel <lak3r@mtmail.mtsu.edu>
2022-09-08 19:07:44 +0000

    getting there

--- Codebase/work_horse.sh
+++ Codebase/work_horse.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
 # $1 is the number of times to run each combo
+corpus='len5_10000-train.txt'
 
 for task in 'SG' 'SA' 'FC' 'NF'
 do
-	for ((x=0; x<$1; x++))
+	for model in LSTM_LSTM LSTM_Transformer Transformer_LSTM Transformer_Transformer
 	do
-		echo $task 
-		echo $x
+		for ((x=0; x<$1; x++))
+		do
+			./single.sh $model'.py' $corpus $task'-10-train.txt' $task'-10-test.txt' $x	
+		done
 	done
+	echo 
 done
