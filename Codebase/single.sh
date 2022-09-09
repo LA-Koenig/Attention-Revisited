f0097015603e8aa2d364928a6ac7c26a74c89080
Laurel <lak3r@mtmail.mtsu.edu>
2022-09-09 04:00:32 +0000

    fake

--- Codebase/single.sh
+++ Codebase/single.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#singularity exec /nfshome/singularity-images/csci4850-2022-Fall.sif $1
+./$1.py $2 $3 $4
+#singularity exec /nfshome/singularity-images/csci4850-2022-Fall.sif ./$1.py $2 $3 $4
 
 # $1 is the program
 # $2 is the corpus 
 # $3 is the training set
 # $4 is the testing set
 
-echo './'$1 $2 $3 $4
+#echo './'$1'.py' $2 $3 $4
--- Codebase/work_horse.sh
+++ Codebase/work_horse.sh
@@ -3,14 +3,30 @@
 # $1 is the number of times to run each combo
 corpus='len5_10000-train.txt'
 
+base=$(pwd)
+mkdir results
+cd results
+
 for task in 'SG' 'SA' 'FC' 'NF'
 do
+	echo 'Task:' $task
 	for model in LSTM_LSTM LSTM_Transformer Transformer_LSTM Transformer_Transformer
 	do
+		echo 'Model:' $model 'running' $1 'times'
+		mkdir -p ./$task/$model
+		cd ./$task/$model
+		pwd
+		echo $base
+		ln -s base/test.py ./test.py 
+		ln -s base/single.sh ./single.sh 
 		for ((x=0; x<$1; x++))
 		do
-			./single.sh $model'.py' $corpus $task'-10-train.txt' $task'-10-test.txt' $x	
+			./single.sh 'test' $corpus $task'-10-train.txt' $task'-10-test.txt'
+			#pwd
+			#./single.sh $model $corpus $task'-10-train.txt' $task'-10-test.txt'
 		done
+		echo
+		cd ../..
 	done
 	echo 
 done
